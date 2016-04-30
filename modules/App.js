eae7f4f1461f7ccf5cb46c5c6b9cc8c0bd2b4e4f
rnjai <rnjailamba12@gmail.com>
2016-04-30 09:30:04 +0530

    IndexLink

--- modules/App.js
+++ modules/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router'
 import NavLink from './NavLink'
+import { IndexLink, Link } from 'react-router'
 
 
 export default React.createClass({
@@ -9,6 +9,7 @@ export default React.createClass({
       <div>
         <h1>Ghettohub Issues</h1>
         <ul role="nav">
+          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
     			<li><NavLink to="/about">About</NavLink></li>
     			<li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
